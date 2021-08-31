480439b671fada602259d15a6f456979f3d0700e
Mick <michnich07@gmail.com>
2021-08-31 18:06:58 -0400

    save search states, subscribe and display in history

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StateService } from './../state.service';
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  history!: Array<any>
+
+  constructor(private state: StateService) { }
 
   ngOnInit(): void {
+    this.state.history$.subscribe(history => {
+      this.history = history;
+    })
   }
 
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
 
 import { NewsService } from './../news.service';
+import { StateService } from './../state.service';
 
 @Component({
   selector: 'app-search',
@@ -20,20 +21,25 @@ export class SearchComponent implements OnInit {
   totalResults!:number;
   results:any = [];
 
-  constructor(private service: NewsService) { }
+  constructor(private news: NewsService, private state: StateService) { }
 
   ngOnInit(): void {
   }
 
   submit(form: NgForm) {
-    this.service.search(this.search, this.type, this.sort, this.time).subscribe((data:any) => {
+    this.news.search(this.search, this.type, this.sort, this.time).subscribe((data:any) => {
       this.results = data.hits;
       this.totalResults = data.nbHits;
+      
+      //save search to history
+      this.state.addToHistory({"term": this.search, "type":this.type, "sort":this.sort, "range":this.time});
+      //save the search state (including the results)
+      this.state.lastSearch(data);
     })
   }
 
   nextPage(event: PageEvent) {
-    this.service.nextSearchPage(this.search, this.type, this.sort, this.time, event.pageIndex).subscribe((data:any) => {
+    this.news.nextSearchPage(this.search, this.type, this.sort, this.time, event.pageIndex).subscribe((data:any) => {
       this.results = data.hits;
     })
   }
