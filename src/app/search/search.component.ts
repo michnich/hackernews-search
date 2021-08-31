1b374812253abd7c602915e6dc21441b4986ac78
Mick <michnich07@gmail.com>
2021-08-31 18:12:14 -0400

    changed variable name search to term, inject state service

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,7 +13,7 @@ import { StateService } from './../state.service';
 })
 export class SearchComponent implements OnInit {
 
-  search = "";
+  term = "";
   type = "all";
   sort = "relevance"; 
   time = "all";
@@ -24,22 +24,23 @@ export class SearchComponent implements OnInit {
   constructor(private news: NewsService, private state: StateService) { }
 
   ngOnInit(): void {
+
   }
 
   submit(form: NgForm) {
-    this.news.search(this.search, this.type, this.sort, this.time).subscribe((data:any) => {
+    this.news.search(this.term, this.type, this.sort, this.time).subscribe((data:any) => {
       this.results = data.hits;
       this.totalResults = data.nbHits;
       
       //save search to history
-      this.state.addToHistory({"term": this.search, "type":this.type, "sort":this.sort, "range":this.time});
+      this.state.addToHistory({"term": this.term, "type":this.type, "sort":this.sort, "range":this.time});
       //save the search state (including the results)
       this.state.lastSearch(data);
     })
   }
 
   nextPage(event: PageEvent) {
-    this.news.nextSearchPage(this.search, this.type, this.sort, this.time, event.pageIndex).subscribe((data:any) => {
+    this.news.nextSearchPage(this.term, this.type, this.sort, this.time, event.pageIndex).subscribe((data:any) => {
       this.results = data.hits;
     })
   }
